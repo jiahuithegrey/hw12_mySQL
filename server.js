8db0f4e59ee20e66ff0ef702342a3e6c3da655c0
Jiahui Wang <wjh4289042@hotmail.com>
2019-12-09 11:33:07 -0800

    build cases

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,7 +15,52 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  readColleges();
+  start();
 });
 
-function readColleges() {
+function start(){
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employees",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee manager",
+            "Exit"
+        ]
+    })
+    .then(function(answer){
+        switch (answer.action){
+            case "View All Employees":
+                viewEmp();
+                break;
+            case "View All Employees By Department":
+                viewEmpByDep();
+                break;
+            case "View All Employees By Manager":
+                viewEmpByMan();
+                break;
+            case "Add Employee":
+                addEmp();
+                break;
+            case "Remove Employee":
+                removeEmp();
+                break;
+            case "Update Employee Role":
+                updateEmpRole();
+                break;
+            case "Update Employee Manager":
+                updateEmpMan();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+        }
+    });
+}
